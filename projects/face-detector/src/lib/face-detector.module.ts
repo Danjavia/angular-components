4f19ddfde86dd4617b8d9566ef69a22c307333e1
Danny Viasus (Danjavia) <danjavia@gmail.com>
2021-04-09 00:05:53 -0500

    #checkpoint

--- projects/face-detector/src/lib/face-detector.module.ts
+++ projects/face-detector/src/lib/face-detector.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { FaceDetectorComponent } from './face-detector.component';
+import {CommonModule} from '@angular/common';
 
 
 
 @NgModule({
   declarations: [FaceDetectorComponent],
   imports: [
+    CommonModule
   ],
   exports: [FaceDetectorComponent]
 })
